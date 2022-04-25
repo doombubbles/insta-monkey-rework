a30177039a04000de9873d5b0fd4beb3dac99ef3
doombubbles <obsidiantoolzg@gmail.com>
2022-04-24 21:48:44 -0700

    Move to new repo

---
+++ ModHelperData.cs
@@ -0,0 +1,15 @@
+﻿namespace InstaMonkeyRework;
+
+public static class ModHelperData
+{
+    public const string Version = "1.0.7";
+    public const string Name = "Insta Monkey Rework";
+
+    public const string Description =
+        "Adds a new way to use insta monkeys: Instead of being one-time use only, " +
+        "you can optionally spend in-game cash for instas and they won't be removed from your collection. " +
+        "Price is discounted from the full amount based on the tier of the insta.";
+
+    public const string RepoOwner = "doombubbles";
+    public const string RepoName = "insta-monkey-rework";
+}
